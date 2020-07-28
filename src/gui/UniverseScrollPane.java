ad714fbd5e7d1f1d0684837df90381810904bd1d
ipythonchik <42135591+ipythonchik@users.noreply.github.com>
2020-07-28 13:54:41 +0300

    fixed unit increment for scrollbars

--- src/gui/UniverseScrollPane.java
+++ src/gui/UniverseScrollPane.java
@@ -7,5 +7,7 @@ public class UniverseScrollPane extends JScrollPane {
     public UniverseScrollPane(Component universePanel) {
         super(universePanel);
         setPreferredSize(new Dimension(900, 600));
+        getVerticalScrollBar().setUnitIncrement(10);
+        getHorizontalScrollBar().setUnitIncrement(10);
     }
 }
